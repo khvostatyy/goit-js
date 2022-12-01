0f884e469bdf2c82eed1fbf66ef332a2dd429272
khvostatyy <gorillochka@gmail.com>
2022-12-01 10:59:46 +0300

    add js hw

--- js/goit.js
+++ js/goit.js
@@ -1,5 +1,64 @@
 // const nameUser = 5;
 // console.log(typeof nameUser);
 
-const shouldRenew = confirm('Хочете подовжити підписку?');
-console.log(shouldRenew);
+// const shouldRenew = confirm('Хочете подовжити підписку?');
+// console.log(shouldRenew);
+
+
+
+// #1
+// const productName = "Droid";
+// const pricePerItem = 2000;
+
+
+// #2
+// let productName = "Droid";
+// let pricePerItem = 2000;
+
+// productName = "Repair droid";
+// pricePerItem = 3500;
+
+// #3
+// const topSpeed = 160;
+// const distance = 617.54;
+
+// const login = "mango935";
+
+// const isOnline = true;
+// const isAdmin = false;
+
+// #4
+// const pricePerItem = 3500;
+// const orderedQuantity = 4;
+
+// const totalPrice = pricePerItem * orderedQuantity;
+
+// #5
+// const productName = "Droid";
+// const pricePerItem = 3500;
+
+// const message = `You picked ${productName}, price per item is ${pricePerItem} credits`
+
+// #6
+// const pricePerDroid = 800;
+// const orderedQuantity = 6;
+// const deliveryFee = 50;
+// const totalPrice = (pricePerDroid * orderedQuantity) + deliveryFee;
+// const message = `You ordered droids worth ${totalPrice} credits. Delivery (${deliveryFee} credits) is included in total price.`;
+
+// #6
+// function sayHi(params) {
+//   console.log("Hello, this is my first function!")
+// }
+
+// sayHi();
+
+// #7
+// function add(a, b, c) {
+//   console.log(`Addition result equals ${a + b + c}`);
+
+// }
+
+// add(15, 27, 10);
+// add(10, 20, 30);
+// add(5, 10, 15);
